82781b9d42f63f60f7b7e2b283aaee054bd18ea2
ZorbaGyrka <empo@abv.bg>
2018-07-14 10:38:09 +0300

    Added shortcuts to New, Open and Save

--- TexticoForm.Designer.cs
+++ TexticoForm.Designer.cs
@@ -28,20 +28,136 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.richTextBox = new System.Windows.Forms.RichTextBox();
+            this.MainMenu = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.FileNew = new System.Windows.Forms.ToolStripMenuItem();
+            this.FileOpen = new System.Windows.Forms.ToolStripMenuItem();
+            this.FileSave = new System.Windows.Forms.ToolStripMenuItem();
+            this.FileSaveAs = new System.Windows.Forms.ToolStripMenuItem();
+            this.FileExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.HelpAbout = new System.Windows.Forms.ToolStripMenuItem();
+            this.MainMenu.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // richTextBox
+            // 
+            this.richTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.richTextBox.Location = new System.Drawing.Point(0, 24);
+            this.richTextBox.Name = "richTextBox";
+            this.richTextBox.Size = new System.Drawing.Size(859, 323);
+            this.richTextBox.TabIndex = 0;
+            this.richTextBox.Text = "This is\nhow a text would\nlook like\n";
+            this.richTextBox.TextChanged += new System.EventHandler(this.richTextBox_TextChanged);
+            // 
+            // MainMenu
+            // 
+            this.MainMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.MainMenu.Location = new System.Drawing.Point(0, 0);
+            this.MainMenu.Name = "MainMenu";
+            this.MainMenu.Size = new System.Drawing.Size(859, 24);
+            this.MainMenu.TabIndex = 1;
+            this.MainMenu.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.FileNew,
+            this.FileOpen,
+            this.FileSave,
+            this.FileSaveAs,
+            this.FileExit});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "&File";
+            // 
+            // FileNew
+            // 
+            this.FileNew.Name = "FileNew";
+            this.FileNew.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.N)));
+            this.FileNew.Size = new System.Drawing.Size(152, 22);
+            this.FileNew.Text = "&New";
+            this.FileNew.Click += new System.EventHandler(this.FileNew_Click);
+            // 
+            // FileOpen
+            // 
+            this.FileOpen.Name = "FileOpen";
+            this.FileOpen.ShortcutKeyDisplayString = "";
+            this.FileOpen.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
+            this.FileOpen.Size = new System.Drawing.Size(152, 22);
+            this.FileOpen.Text = "&Open";
+            this.FileOpen.Click += new System.EventHandler(this.FileOpen_Click);
+            // 
+            // FileSave
+            // 
+            this.FileSave.Name = "FileSave";
+            this.FileSave.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S)));
+            this.FileSave.Size = new System.Drawing.Size(152, 22);
+            this.FileSave.Text = "&Save";
+            this.FileSave.Click += new System.EventHandler(this.FileSave_Click);
+            // 
+            // FileSaveAs
+            // 
+            this.FileSaveAs.Name = "FileSaveAs";
+            this.FileSaveAs.Size = new System.Drawing.Size(152, 22);
+            this.FileSaveAs.Text = "Save &as";
+            this.FileSaveAs.Click += new System.EventHandler(this.FileSaveAs_Click);
+            // 
+            // FileExit
+            // 
+            this.FileExit.Name = "FileExit";
+            this.FileExit.Size = new System.Drawing.Size(152, 22);
+            this.FileExit.Text = "E&xit";
+            this.FileExit.Click += new System.EventHandler(this.FileExit_Click);
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.HelpAbout});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "&Help";
+            // 
+            // HelpAbout
+            // 
+            this.HelpAbout.Name = "HelpAbout";
+            this.HelpAbout.Size = new System.Drawing.Size(152, 22);
+            this.HelpAbout.Text = "&About";
+            this.HelpAbout.Click += new System.EventHandler(this.HelpAbout_Click);
             // 
             // TexticoForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(859, 347);
+            this.Controls.Add(this.richTextBox);
+            this.Controls.Add(this.MainMenu);
+            this.MainMenuStrip = this.MainMenu;
             this.Name = "TexticoForm";
             this.Text = "Textico v1.0";
+            this.MainMenu.ResumeLayout(false);
+            this.MainMenu.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox;
+        private System.Windows.Forms.MenuStrip MainMenu;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem FileNew;
+        private System.Windows.Forms.ToolStripMenuItem FileOpen;
+        private System.Windows.Forms.ToolStripMenuItem FileSave;
+        private System.Windows.Forms.ToolStripMenuItem FileSaveAs;
+        private System.Windows.Forms.ToolStripMenuItem FileExit;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem HelpAbout;
     }
 }
 
--- TexticoForm.cs
+++ TexticoForm.cs
@@ -3,18 +3,138 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static Textico.TexticoForm.ESaveChanges;
 
 namespace Textico
 {
     public partial class TexticoForm : Form
     {
+        string TitleText = "Textico v1.0";
+        string NoFileName = "Untitled";
+        string FileName = "";
+        string SavedContent = "";
+        string FileFilter = "Text files(.txt)|*.txt|All files|*.*";
+
+        public enum ESaveChanges { Continue, Abort }
         public TexticoForm()
         {
+            FileName = NoFileName;
             InitializeComponent();
+            richTextBox.Clear();
+            UpdateTitle();
+        }
+
+        ESaveChanges AskAndSaveChanges()
+        {
+            if (!HasChanges()) return Continue;
+            var choice = MessageBox.Show("Save changes" + (!String.IsNullOrEmpty(FileName) ? " to " + FileName : ""), "Save",
+                MessageBoxButtons.YesNoCancel);
+            if (choice == DialogResult.Cancel)
+                return Abort;
+            if (choice != DialogResult.Yes)
+                return Continue;
+
+            SaveChanges();
+            return Continue;
+        }
+
+        private void SaveChanges()
+        {
+            if (!String.IsNullOrEmpty(FileName) && FileName != NoFileName)
+                SaveFile(FileName);
+            else
+                SaveAs();
+        }
+
+        private void SaveFile(string fileName)
+        {
+            SavedContent = richTextBox.Text;
+            File.WriteAllText(fileName, richTextBox.Text);
+            UpdateTitle();
+        }
+
+        private void SaveAs()
+        {
+            var dlg = new SaveFileDialog();
+            dlg.FileName = FileNameWithoutPath(FileName);
+            dlg.Filter = FileFilter;
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                FileName = dlg.FileName;
+                SaveFile(FileName);
+            }
+        }
+
+        private void UpdateTitle()
+        {
+            Text = (HasChanges() ? "*" : "") + FileNameWithoutPath(FileName) + (String.IsNullOrEmpty(FileName) ? "" : " - ") + TitleText;
+        }
+
+        private bool HasChanges()
+        {
+            return SavedContent != richTextBox.Text;
+        }
+
+        string FileNameWithoutPath(string fileName)
+        {
+            var slashIndex = fileName.LastIndexOf("\\");
+            return slashIndex < 0 ? fileName : fileName.Substring(slashIndex + 1);
+        }
+
+
+        #region Menu commands
+
+        private void HelpAbout_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("A very simple text editor by ZorbaGyrka");
+        }
+
+        private void FileNew_Click(object sender, EventArgs e)
+        {
+            if (AskAndSaveChanges() == Abort)
+                return;
+            FileName = NoFileName;
+            SavedContent = "";
+            richTextBox.Clear();
+            UpdateTitle();
+        }
+
+        private void FileOpen_Click(object sender, EventArgs e)
+        {
+            var dlg = new OpenFileDialog();
+            dlg.Filter = FileFilter;
+            if (dlg.ShowDialog() != DialogResult.OK)
+                return;
+            FileName = dlg.FileName;
+            SavedContent = File.ReadAllText(FileName);
+            richTextBox.Text = SavedContent;
+            UpdateTitle();
+        }
+
+        private void FileSave_Click(object sender, EventArgs e)
+        {
+            SaveChanges();
+        }
+
+        private void FileSaveAs_Click(object sender, EventArgs e)
+        {
+            SaveAs();
+        }
+
+        private void FileExit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+        #endregion
+
+        private void richTextBox_TextChanged(object sender, EventArgs e)
+        {
+            UpdateTitle();
         }
     }
 }
